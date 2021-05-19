c541e90bd5c53ca8f1f3c9c5b712b0e340ed6a3f
Mel <meljohnpualon@gmail.com>
2021-05-20 00:16:44 +0800

    fetch array objects from inline array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from './components/Header'
-
+import Tasks from './components/Tasks'
 
 function App() {
   return (
     <div className="container">
       <Header title={'Task Tracker'} ></Header>
-      
+      <Tasks ></Tasks>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,5 +1,36 @@
-// const Tasks = () => {
-//     return (  );
-// }
+const tasks = [
+    {
+        id: 1,
+        text: 'Coding Session',
+        day: 'everyday',
+        reminder: 'true'
+    },
+    {
+        id: 2,
+        text: 'CSS Session',
+        day: 'Monday',
+        reminder: 'true'
+    },
+    {
+        id: 3,
+        text: 'relaxing',
+        day: 'sunday',
+        reminder: 'false'
+    },
+];
+
+const Tasks = () => {
+    return ( 
+        <>
+            {/* {tasks.map(task => (
+                <h3>{task.text}</h3>
+            ))} */}
+
+            {/* {tasks.map((task) => (<h3>{task.text}</h3>))} */}
+
+            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+        </>
+     );
+}
  
-// export default Tasks;
+export default Tasks;
