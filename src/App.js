3418e052c0a44b7f2f2913147c2ca930158b8807
Mel <meljohnpualon@gmail.com>
2021-05-19 08:38:00 +0800

    added button sample component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import Button from './components/Buttons'
 
 function App() {
   return (
     <div className="container">
       <Header title={'Task Tracker'}></Header>
+      <Button className={'btn'}></Button>
     </div>
   );
 }
---
+++ src/components/Buttons.js
@@ -0,0 +1,7 @@
+const Button = ({className}) => {
+    return ( 
+        <button className={className}>add</button>
+    );
+}
+ 
+export default Button;
