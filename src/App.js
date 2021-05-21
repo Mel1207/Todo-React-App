88cde07823d99b18a0f4a2d09e498002c62cb428
Mel <meljohnpualon@gmail.com>
2021-05-22 05:29:44 +0800

    added fetch task func from db

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AddTask from './components/AddTask';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
@@ -7,6 +7,25 @@ function App() {
   const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTask = async () => {
+      const taskFromServer = await fetchTask()
+      setTasks(taskFromServer)
+    }
+
+    getTask();
+  }, [])
+
+  // Fetch task
+  const fetchTask = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
+  fetchTask()
 
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
